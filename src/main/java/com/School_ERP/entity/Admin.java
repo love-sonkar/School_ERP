dd04e18a46373e05b475c0aea0243fbf34dd2e54
afshan234 <afshan234@gmail.com>
2024-06-10 17:47:10 +0530

    swagger ui added

--- src/main/java/com/School_ERP/entity/Admin.java
+++ src/main/java/com/School_ERP/entity/Admin.java
@@ -1,5 +1,6 @@
 package com.School_ERP.entity;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -12,10 +13,17 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Schema(description = "Entity representing an Admin in the system")
 public class Admin {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Schema(description = "Unique identifier of the admin", example = "1")
     private Long id;
+
+    @Schema(description = "Username of the admin", example = "admin123")
     private String userName;
+
+    @Schema(description = "Password of the admin", example = "password")
     private String password;
 }
