5bdf56902e954d4f97464123d379104eee47d30f
love-sonkar <loveboss806@gmail.com>
2024-06-11 13:34:54 +0530

    fixed conflict

--- src/main/java/com/School_ERP/service/AttendenceService.java
+++ src/main/java/com/School_ERP/service/AttendenceService.java
@@ -1,11 +1,8 @@
 package com.School_ERP.service;
 
 import com.School_ERP.dto.AttendenceDto;
-import com.School_ERP.entity.Attendence;
 import org.springframework.http.ResponseEntity;
-import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.PutMapping;
 
 import java.util.List;
 
@@ -13,8 +10,6 @@ import java.util.List;
 public interface AttendenceService {
 
     List<AttendenceDto> getAllAttendance();
-
-    List<AttendenceDto> getAttendanceByRollNo();
 
     ResponseEntity<?> getOverView(long rollNo);
 }
